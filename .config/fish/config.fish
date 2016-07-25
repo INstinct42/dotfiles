a0f935aedc4a82b2fff687bd2f7fd065e0c7a254
Jonas Weissensel <weissensel@maritimedatasystems.com>
2016-07-25 15:16:12 +0200

    Fish config update pc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,20 @@
-set fisher_home ~/.local/share/fisherman
+set fisher_home ~/.config/fisherman
 set fisher_config ~/.config/fisherman
-source $fisher_home/config.fish
+source $fisher_home/key_bindings.fish
 set -x PATH /home/jonas/bin $PATH
-set -x EDITOR "emacsclient -nw -a ''"
-set -x GDK_SCALE 2
-set -x QT_DEVICE_PIXEL_RATIO 2
-set -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent.ssh
+set -x EDITOR "nvim"
+# set -x GDK_SCALE 2
+# set -x QT_DEVICE_PIXEL_RATIO 2
+# set -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent.ssh
+bass (gnome-keyring-daemon --start --components=ssh)
 set -x TERM xterm-256color
 set -x FZF_DEFAULT_COMMAND "find -L * -path '*/\.*' -prune -o -type f -print -o -type l -print 2> /dev/null"
+eval (python -m virtualfish)
+
+for f in ~/.config/fish/conf.d/*.fish
+    source $f
+end
+
+function fish_title
+    true
+end
